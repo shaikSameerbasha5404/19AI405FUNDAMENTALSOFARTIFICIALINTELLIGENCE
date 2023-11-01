09ef81bec1878d96bec76e08ba612e3eae68f6d4
SHAIK SAMEER BASHA <118707756+shaikSameerbasha5404@users.noreply.github.com>
2023-11-01 16:08:39 +0530

    Update AStarAlgorithm.py

--- ExpNo 3/AStarAlgorithm.py
+++ ExpNo 3/AStarAlgorithm.py
@@ -1,3 +1,57 @@
+<h1>ExpNo 3 : Implement A* search algorithm for a Graph</h1> 
+<h3>Name: Shaik Sameer Basha</h3>
+<h3>Register Number: 212222240093</h3>
+<H3>Aim:</H3>
+<p>To ImplementA * Search algorithm for a Graph using Python 3.</p>
+<H3>Algorithm:</H3>
+// A* Search Algorithm
+1.  Initialize the open list.
+
+2.  Initialize the closed list
+    put the starting node on the open 
+    list (you can leave its f at zero)
+    
+
+4.  while the open list is not empty
+    a) find the node with the least f on 
+       the open list, call it "q"
+
+    b) pop q off the open list
+  
+    c) generate q's 8 successors and set their 
+       parents to q
+    
+   
+    d) for each successor
+    
+        i) if successor is the goal, stop search
+        
+        ii) else, compute both g and h for successor
+          successor.g = q.g + distance between 
+                              successor and q
+          successor.h = distance from goal to 
+          successor (This can be done using many 
+          ways, we will discuss three heuristics- 
+          Manhattan, Diagonal and Euclidean 
+          Heuristics)
+          
+          successor.f = successor.g + successor.h
+
+        iii) if a node with the same position as 
+            successor is in the OPEN list which has a 
+           lower f than successor, skip this successor
+
+        iV) if a node with the same position as 
+            successor  is in the CLOSED list which has
+            a lower f than successor, skip this successor
+            otherwise, add  the node to the open list
+     end (for loop)
+  
+    e) push q on the closed list
+    end (while loop)
+
+<h2>Program:</h2>
+<pre class="poem">
 from collections import defaultdict
 H_dist ={}
 def aStarAlgo(start_node, stop_node):
@@ -69,21 +123,7 @@ def get_neighbors(v):
         return None
 def heuristic(n):
     return H_dist[n]
-
-
-#Describe your graph here
-'''Graph_nodes = {
-    'A': [('B', 6), ('F', 3)],
-    'B': [('A', 6), ('C', 3), ('D', 2)],
-    'C': [('B', 3), ('D', 1), ('E', 5)],
-    'D': [('B', 2), ('C', 1), ('E', 8)],
-    'E': [('C', 5), ('D', 8), ('I', 5), ('J', 5)],
-    'F': [('A', 3), ('G', 1), ('H', 7)],
-    'G': [('F', 1), ('I', 3)],
-    'H': [('F', 7), ('I', 2)],
-    'I': [('E', 5), ('G', 3), ('H', 2), ('J', 3)],
-}
-'''
+    
 graph = defaultdict(list)
 n,e = map(int,input().split())
 for i in range(e):
@@ -102,3 +142,76 @@ Graph_nodes=graph
 print(graph)
 aStarAlgo('S', 'G')
 
+</pre>
+<hr>
+<h2>Sample Graph I</h2>
+<hr>
+
+![image](https://github.com/natsaravanan/19AI405FUNDAMENTALSOFARTIFICIALINTELLIGENCE/assets/87870499/b1377c3f-011a-4c0f-a843-516842ae056a)
+
+<hr>
+<h2>Sample Input</h2>
+<hr>
+10 14 <br>
+A B 6 <br>
+A F 3 <br>
+B D 2 <br>
+B C 3 <br>
+C D 1 <br>
+C E 5 <br>
+D E 8 <br>
+E I 5 <br>
+E J 5 <br>
+F G 1 <br>
+G I 3 <br>
+I J 3 <br>
+F H 7 <br>
+I H 2 <br>
+A 10 <br>
+B 8 <br>
+C 5 <br>
+D 7 <br>
+E 3 <br>
+F 6 <br>
+G 5 <br>
+H 3 <br>
+I 1 <br>
+J 0 <br>
+<hr>
+<h2>Sample Output</h2>
+<hr>
+Path found: ['A', 'F', 'G', 'I', 'J']
+
+
+<hr>
+<h2>Sample Graph II</h2>
+<hr>
+
+![image](https://github.com/natsaravanan/19AI405FUNDAMENTALSOFARTIFICIALINTELLIGENCE/assets/87870499/acbb09cb-ed39-48e5-a59b-2f8d61b978a3)
+
+
+<hr>
+<h2>Sample Input</h2>
+<hr>
+6 6 <br>
+A B 2 <br>
+B C 1 <br>
+A E 3 <br>
+B G 9 <br>
+E D 6 <br>
+D G 1 <br>
+A 11 <br>
+B 6 <br>
+C 99 <br>
+E 7 <br>
+D 1 <br>
+G 0 <br>
+<hr>
+<h2>Sample Output</h2>
+<hr>
+Path found: ['A', 'E', 'D', 'G']
+
+<hr>
+<h2>Result:</h2>
+Thus, the implementation of A * Search algorithm for a Graph using Python 3. is successfully executed.
+<hr>
